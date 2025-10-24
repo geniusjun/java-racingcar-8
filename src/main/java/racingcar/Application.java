f0e7d10f04a567d3c43d34d2ddef13613acdd764
geniusjun <geniusjun4663@gmail.com>
2025-10-24 15:24:12 +0900

    Feat(RaceController): view->controller 입력 값 전달 및 InputView 의존성 추가
    뷰에서는 빈 값만 검증 후 순수한 문자열을 컨트롤러 쪽으로 전달해주고, 생성자를 통한 의존성 주입을 구현한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,12 @@
 package racingcar;
 
 import racingcar.controller.RaceController;
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        RaceController raceController = new RaceController(new OutputView());
+        RaceController raceController = new RaceController(new OutputView(), new InputView());
         raceController.start();
     }
 }
--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,16 +1,20 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RaceController {
 
     private final OutputView outputView;
+    private final InputView inputView;
 
-    public RaceController(OutputView outputView) {
+    public RaceController(OutputView outputView, InputView inputView) {
         this.outputView = outputView;
+        this.inputView = inputView;
     }
 
     public void start() {
         outputView.printStart();
+        String message = inputView.requestLine(); // 임시
     }
 }
