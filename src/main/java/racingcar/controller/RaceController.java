a9ca14cc4e3bfc7a20a4535f1dc7271dea084b09
geniusjun <geniusjun4663@gmail.com>
2025-10-25 19:36:21 +0900

    Feat(RaceController): 전진 시도할 횟수를 입력받는 기능
    전진 시도할 횟수를 입력받는 메서드를 따로 빼서 함수의 depth를 줄이고자 한다.

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,6 +1,8 @@
 package racingcar.controller;
 
 import racingcar.domain.Cars;
+import racingcar.global.CustomArgumentException;
+import racingcar.global.constans.ErrorMessage;
 import racingcar.global.constans.MessageType;
 import racingcar.view.View;
 
@@ -17,5 +19,18 @@ public class RaceController {
         Cars cars = Cars.from(view.enterMessage());
 
         view.printlnMessage(MessageType.ADVANCE_MESSAGE);
+        int count = advanceCount();
+        while (count-- > 0) {
+
+        }
+
+    }
+
+    private int advanceCount() {
+        try {
+            return Integer.parseInt(view.enterMessage());
+        } catch (NumberFormatException e) {
+            throw CustomArgumentException.from(ErrorMessage.SYSTEM_ERROR);
+        }
     }
 }
--- src/main/java/racingcar/global/constans/ErrorMessage.java
+++ src/main/java/racingcar/global/constans/ErrorMessage.java
@@ -3,7 +3,8 @@ package racingcar.global.constans;
 public enum ErrorMessage {
     BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다."),
     NAME_BLANK_ERROR("자동차 이름은 빈 값을 허용하지 않습니다."),
-    NAME_LENGTH_ERROR("자동차의 이름은 이름은 5자 이하만 가능합니다.");
+    NAME_LENGTH_ERROR("자동차의 이름은 이름은 5자 이하만 가능합니다."),
+    SYSTEM_ERROR("알 수 없는 오류입니다.");
     private final String message;
 
     ErrorMessage(String message) {
