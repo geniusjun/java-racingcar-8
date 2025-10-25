d9ad9b5ac4dfa463644abf88dd41bb65ea69dbef
geniusjun <geniusjun4663@gmail.com>
2025-10-25 16:14:04 +0900

    Feat(Car): 경주할 자동차 이름과 전진 횟수를 보관할 객체 Car 생성
    정적 팩토리 메서드를 활용하여 후에 추가할 검증을 대비한다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int advance;
+
+    private Car(String name) {
+        this.name = name;
+        this.advance = 0;
+    }
+
+    public static Car from(String name) {
+        return new Car(name);
+    }
+}
