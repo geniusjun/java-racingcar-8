98400913bc5644dbf7cd0ca1babcff2a6f16a67c
geniusjun <geniusjun4663@gmail.com>
2025-10-25 17:25:52 +0900

    Feat(Cars): 콤마를 구분자로 이름을 나누어 Car객체를 만들어 Cars에 저장
    정적 중첩 클래스를 활용하여 들어온 문자열을 콤마로 구분하여 Car 객체를 만들고, 이를 일급 콜렉션 Cars 멤버변수에 저장한다.

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,6 +12,21 @@ public class Cars {
     }
 
     public static Cars from(String input) {
-        return new Cars(Arrays.asList(Car.from(input))); // 임시
+        return new Cars(Parser.split(input));
+    }
+
+    private static class Parser {
+        public static List<Car> split(String input) {
+            List<String> names = Arrays.stream(input.split(",")).toList();
+            return makeCars(names);
+        }
+
+        private static List<Car> makeCars(List<String> names) {
+            List<Car> carList = new ArrayList<>();
+            for (String name : names) {
+                carList.add(Car.from(name));
+            }
+            return carList;
+        }
     }
 }
