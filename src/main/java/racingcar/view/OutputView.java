5dd6d5757e2e57c4bdbb075b91900dfba82fd120
geniusjun <geniusjun4663@gmail.com>
2025-10-26 20:21:39 +0900

    Refactor(OutputView): 출력 형식 상수화

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,6 +4,9 @@ import java.util.List;
 import racingcar.global.constans.MessageType;
 
 public class OutputView {
+    private static final String RESULT_DELIMITER = " : ";
+    private static final String RESULT_POSITION_UNIT = "-";
+    private static final String FINAL_RESULT_DELIMITER = ", ";
 
     public void printlnMessage(MessageType messageType) {
         System.out.println(messageType.getMessage());
@@ -14,11 +17,11 @@ public class OutputView {
     }
 
     public void printResult(String name, int advance) {
-        System.out.println(name + " : " + "-".repeat(advance));
+        System.out.println(name + RESULT_DELIMITER + RESULT_POSITION_UNIT.repeat(advance));
     }
 
     public void printWinner(List<String> list) {
-        System.out.println(String.join(", ", list));
+        System.out.println(String.join(FINAL_RESULT_DELIMITER, list));
     }
 }
 
