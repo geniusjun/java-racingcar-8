279ddc4144ea792870ab77864c72f6b058d43108
geniusjun <geniusjun4663@gmail.com>
2025-10-27 10:14:26 +0900

    Test(InputTest): 이름 입력 시 원시값(문자열) 그대로 잘 반환하는지 테스트

--- src/test/java/racingcar/view/InputTest.java
+++ src/test/java/racingcar/view/InputTest.java
@@ -1,8 +1,12 @@
 package racingcar.view;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class InputTest {
 
@@ -15,4 +19,17 @@ public class InputTest {
         Console.close();
     }
 
+    @Test
+    @DisplayName("이름 입력: 정상 문자열을 그대로 반환한다")
+    void enterMessage_ok() {
+        // given
+        setStdin("pobi,woni\n");
+        InputView view = new InputView();
+
+        // when
+        String names = view.enterMessage();
+
+        // then
+        assertEquals("pobi,woni", names);
+    }
 }
